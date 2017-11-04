385c52248b3b1b7c4c7a67fa8acdabe20e26c74f
Amir Nadiri <amir77ni@gmail.com>
2017-11-04 20:51:51 +0330

    Update Context.java

--- Models/Context.java
+++ Models/Context.java
@@ -1,15 +1,19 @@
 package Chess.Models;
 
+import Chess.Models.Board;
+import Chess.Models.Player;
+
 class Context {
 	
 	private Board CurrentBoard;
 		
-	private Player[] CurrentPlayers;
+	private ArrayList<Player> CurrentPlayers;
 	
-	public void Context(Player[] players)
+	public void Context(Player player1, Player player2)
    	{
 		this.CurrentBoard = new Board();
-		this.CurrentPlayers = players;
+		this.CurrentPlayers.add(player1);
+		this.CurrentPlayers.add(player2);
 	}
 	
 }
